7b35a663afae312a105ab18ee6ae5989cbc40517
Drew-Kirby <159677238+Drew-Kirby@users.noreply.github.com>
2026-07-22 15:30:48 -0400

    Functionality works. Run the script in Build.bat to create an exe which will be located in a folder titled, "dist".

--- QuoteBuilder/app.py
+++ QuoteBuilder/app.py
@@ -1,3 +1,17 @@
+import os
+import sys
 import webview
-webview.create_window('Test', 'index.html')
+
+def get_asset_path(relative_path):
+    """Get absolute path to resource, works for dev and for PyInstaller"""
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+html_path = get_asset_path('index.html')
+
+webview.create_window('Quick Quote', html_path)
 webview.start()
---
+++ build.bat
@@ -0,0 +1,2 @@
+py -m PyInstaller --noconsole --onefile --clean --icon="app_icon.ico" --add-data "index.html;." --add-data "script.js;." --add-data "style.css;." app.py
+pause
