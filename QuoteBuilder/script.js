1bba15b3bc9d9a5f8cb52a082bb347c503abd797
Drew Kirby <159677238+Drew-Kirby@users.noreply.github.com>
2026-07-22 14:07:08 -0400

    Initial files I created before I created the repo.

---
+++ QuoteBuilder/script.js
@@ -0,0 +1,6 @@
+const totSale = 0;
+
+submitBtn.addEventListener("click")
+
+document.getElementById("totCost").innerHTML = `<h2>$${totCost}</h2>`;
+document.getElementById("totSale").innerHTML = `<h2>$${totSale}</h2>`;
